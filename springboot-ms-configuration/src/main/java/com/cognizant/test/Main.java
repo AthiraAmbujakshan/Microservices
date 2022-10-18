1f3524fd7344711bb66ba68dbf07fcf4f05c06a8
AthiraAmbujakshan <63126142+AthiraAmbujakshan@users.noreply.github.com>
2022-10-18 21:17:40 +0530

    Create Main.java

---
+++ springboot-ms-configuration/src/main/java/com/cognizant/test/Main.java
@@ -0,0 +1,13 @@
+package com.cognizant.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMsConfigurationApplication.class, args);
+	}
+
+}
